e88d563cfe9a31c807fd5508ef3e9daf8a5ed9ba
Matthew Long <cathroad18@gmail.com>
2021-02-25 17:03:13 -0600

    Main Menu Testing

---
+++ src/main/java/edu/uark/registerapp/controllers/MainMenuRouteController.java
@@ -0,0 +1,41 @@
+package edu.uark.registerapp.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import edu.uark.registerapp.commands.products.ProductsQuery;
+import edu.uark.registerapp.controllers.enums.ViewModelNames;
+import edu.uark.registerapp.controllers.enums.ViewNames;
+import edu.uark.registerapp.models.api.Product;
+
+@Controller
+@RequestMapping(value = "/mainMenu")
+public class MainMenuRouteController {
+	@RequestMapping(method = RequestMethod.GET)
+	public ModelAndView showProductListing() {
+		ModelAndView modelAndView =
+			new ModelAndView(ViewNames.MAIN_MENU.getViewName());
+
+		try {
+			modelAndView.addObject(
+				ViewModelNames.PRODUCTS.getValue(),
+				this.productsQuery.execute());
+		} catch (final Exception e) {
+			modelAndView.addObject(
+				ViewModelNames.ERROR_MESSAGE.getValue(),
+				e.getMessage());
+			modelAndView.addObject(
+				ViewModelNames.PRODUCTS.getValue(),
+				(new Product[0]));
+		}
+		
+		return modelAndView;
+	}
+
+	// Properties
+	@Autowired
+	private ProductsQuery productsQuery;
+}
