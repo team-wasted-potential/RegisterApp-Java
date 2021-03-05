9df84784f443c29ccdcc26a4ca682e12452ae2cf
laurengjacks <77463861+laurengjacks@users.noreply.github.com>
2021-03-04 19:34:51 -0600

    Create EmployeeSignIn.java

---
+++ src/main/java/edu/uark/registerapp/models/api/EmployeeSignIn.java
@@ -0,0 +1,19 @@
+package edu.uark.registerapp.models.api;
+
+
+public class EmployeeSignIn 
+{
+    String employeeId = "";
+    String password = "";
+
+    public String getEmployeeId()
+    {
+        return this.employeeId;
+    }
+
+    public String getPassword()
+    {
+        return this.password;
+    }
+
+}
