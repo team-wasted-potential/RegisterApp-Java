9b3b692610b74cc50e5bc29759c857f7616b06a0
Matthew Long <mlong@PSI.COM>
2021-03-05 08:48:45 -0600

    Main Menu Server Side

--- src/main/java/edu/uark/registerapp/controllers/MainMenuRouteController.java
+++ src/main/java/edu/uark/registerapp/controllers/MainMenuRouteController.java
@@ -5,18 +5,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-
-
+import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
 import edu.uark.registerapp.controllers.enums.ViewNames;
 
 @Controller
 @RequestMapping(value = "/")
 public class MainMenuRouteController {
 	@GetMapping("")
-	 public ModelAndView mainMenu() {
-		ModelAndView modelAndView =
-			new ModelAndView(ViewNames.MAIN_MENU.getViewName());
-		
+	public ModelAndView mainMenu(@RequestParam final Map<String, String> queryParameters, final HttpServletRequest request) {
+		ModelAndView modelAndView =	new ModelAndView(ViewNames.MAIN_MENU.getViewName());
+
 		return modelAndView;
 	}
 	public String getQuery(@RequestParam String query)
