304d24cc33b27c5d11f7cf694deaac066f1332d6
Matthew Long <cathroad18@gmail.com>
2021-04-09 09:40:04 -0500

    Fixed RouteControllers

--- src/main/java/edu/uark/registerapp/controllers/ProductListingRouteController.java
+++ src/main/java/edu/uark/registerapp/controllers/ProductListingRouteController.java
@@ -1,24 +1,38 @@
 package edu.uark.registerapp.controllers;
-
+import java.util.Map;
+import java.util.Optional;
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-
 import edu.uark.registerapp.commands.products.ProductsQuery;
 import edu.uark.registerapp.controllers.enums.ViewModelNames;
 import edu.uark.registerapp.controllers.enums.ViewNames;
 import edu.uark.registerapp.models.api.Product;
-
+import edu.uark.registerapp.models.entities.ActiveUserEntity;
 @Controller
 @RequestMapping(value = "/productListing")
-public class ProductListingRouteController {
+public class ProductListingRouteController extends BaseRouteController {
 	@RequestMapping(method = RequestMethod.GET)
-	public ModelAndView showProductListing() {
+	public ModelAndView showProductListing(
+		@RequestParam final Map<String, String> queryParameters,
+		final HttpServletRequest request
+	) {
+		final Optional<ActiveUserEntity> activeUserEntity =
+			this.getCurrentUser(request);
+		if (!activeUserEntity.isPresent()) {
+			return buildInvalidSessionResponse();
+		}
 		ModelAndView modelAndView =
-			new ModelAndView(ViewNames.PRODUCT_LISTING.getViewName());
-
+			this.setErrorMessageFromQueryString(
+				new ModelAndView(ViewNames.PRODUCT_LISTING.getViewName()),
+				queryParameters);
+		modelAndView.addObject(
+			ViewModelNames.IS_ELEVATED_USER.getValue(),
+			this.isElevatedUser(activeUserEntity.get()));
 		try {
 			modelAndView.addObject(
 				ViewModelNames.PRODUCTS.getValue(),
@@ -31,10 +45,8 @@ public class ProductListingRouteController {
 				ViewModelNames.PRODUCTS.getValue(),
 				(new Product[0]));
 		}
-		
 		return modelAndView;
 	}
-
 	// Properties
 	@Autowired
 	private ProductsQuery productsQuery;
